0438949c33f0f80d6b5ee6256cccfb385118db5d
Ldeng928 <lideng928@gmail.com>
2021-09-23 10:12:02 -0400

    Initilized project with basic Create-React-App

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React , {Fragment}from 'react'
+
+export const About = () => {
+    return (
+        <Fragment>
+            <h1>About this APP</h1>
+            <p>APP to search GitHub users.</p>
+            <p>Version: 1.0.0</p>
+        </Fragment>                
+    )
+}
